285cab8aee4dcea6fbf84c7c382992d70334483a
Emmanuel Frecon <emmanuel.frecon@myjoice.com>
2016-06-17 15:15:46 +0200

    Adding a plugin to collect file/directory statistics into variables.

--- lib/concocter/plugins.spc
+++ lib/concocter/plugins.spc
@@ -3,4 +3,5 @@
 @*docker+*://* %libdir%/plugins/docker.tcl
 @* %libdir%/plugins/file.tcl
 =* %libdir%/plugins/expr.tcl
+^* %libdir%/plugins/stats.tcl
 * %libdir%/plugins/mapper.tcl
---
+++ lib/concocter/plugins/stats.tcl
@@ -0,0 +1,23 @@
+namespace eval ::concocter::var::plugin::stats {
+    namespace eval gvals {
+    }
+    namespace import [namespace parent [namespace parent]]::setvar
+    namespace import [namespace parent [namespace parent]]::new
+}
+
+proc ::concocter::var::plugin::stats::update { var location } {
+    variable gvals
+    
+    upvar \#0 $var VAR
+    
+    set updated 0
+    set location [string trim [string range $location 1 end]]
+    set location [::utils::resolve $location]
+    ::utils::debug DEBUG "Getting statistics for $location"
+    if { [catch {file stat $location stats} err] == 0 } {
+        set updated [setvar $var [array get stats]]
+    } else {
+        ::utils::debug ERROR "Cannot get statistics for $location: $err"
+    }
+    return $updated
+}
